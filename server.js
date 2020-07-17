7a2b351979858a04267106aae2a0c5adab9be3ce
MESoliman <seliman1611@gmail.com>
2020-07-17 20:23:20 +0200

    changed

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.MONGO_URI || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
